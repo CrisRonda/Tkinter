e488212dc64796a16fcbf1e81cf393ce43ff2c9a
CrisRonda <cristian.j005@gmail.com>
2020-02-27 20:37:15 -0500

    feat: calculator.py

--- README.md
+++ README.md
@@ -5,4 +5,17 @@ Para instalar Syder utiliza los siguientes comandos con Python 3.x:
 - `sudo apt-get update`
 - `sudo apt-get install spyder`
 - `sudo pip install spyder`
+# Screenshots
+Entrada de textos
+<div align="center">
+  <img src="./screenshots/entry.png" width="50%" style="align: center">
+</div>
+<div align="center">
+  <img src="./screenshots/entryLarge.png" width="50%" style="align: center">
+</div>
 
+`Calculator.py`
+
+<div align="center">
+  <img src="./screenshots/add.png" width="150px" style="align: center">
+</div>
--- index.py
+++ index.py
@@ -1,8 +1,14 @@
 from tkinter import *
 
+
+def hola():
+    print("Hola mundo!")
+
+
 # Configuración de la raíz
 root = Tk()
-
+root.title("Primer Programa con Tkinter")
+root.resizable(0, 0)
 label = Label(root, text="Nombre de usuario")
 label.grid(row=0, column=0, sticky="w", padx=5, pady=5)
 
@@ -16,6 +22,14 @@ label2.grid(row=1, column=0, sticky="w", padx=5, pady=5)
 entry2 = Entry(root)
 entry2.grid(row=1, column=1, padx=5, pady=5)
 entry2.config(justify="center", show="*")
+
+label3 = Label(root, text="Ingresa un párrafo")
+label.grid(row=4, column=0, padx=16, pady=16)
+texto = Text(root)
+texto.grid(row=5, column=0, padx=16, pady=16)
+texto.config(width=30, height=10, font=("Consolas", 12),
+             padx=15, pady=15, selectbackground="red")
+
 
 # Finalmente bucle de la aplicación
 root.mainloop()
